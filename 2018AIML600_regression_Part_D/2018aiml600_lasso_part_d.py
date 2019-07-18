a651ea6916cab54b2b16355dd9d61a9e74cde322
vickie026 <break.the.rules26@gmail.com>
2019-07-18 12:39:21 +0530

    lasso and ridge regression

--- 2018AIML600_regression_Part_D/2018aiml600_lasso_part_d.py
+++ 2018AIML600_regression_Part_D/2018aiml600_lasso_part_d.py
@@ -4,27 +4,41 @@ from math import sqrt
 
 from sklearn.preprocessing import PolynomialFeatures
 
-#Defining a polynomial regressor
-def polynomial_regressor(deg, X_train, X_test, y_train, y_test) :
-    poly_reg = PolynomialFeatures(degree = deg)
+
+# Defining a lasso polynomial regressor
+def polynomial_lasso_regressor(deg, X_train, X_test, y_train, y_test) :
+    poly_reg = PolynomialFeatures(degree=deg)
     X_poly = poly_reg.fit_transform(X_train)
     X_test_poly = poly_reg.fit_transform(X_test)
     poly_reg.fit(X_poly, y_train)
-    from sklearn.linear_model import LinearRegression
-    lin_reg = LinearRegression()
-    lin_reg.fit(X_poly, y_train)
+    #creating lasso regressor with default alpha
+    from sklearn.linear_model import Lasso
+    lasso = Lasso(max_iter=1e6)
+    lasso.fit(X_poly, y_train)
+    # Predicting training and testing values
+    y_predicted = lasso.predict(X_poly)
+    y_test_predicted = lasso.predict(X_test_poly)
 
-    #Predicting training and testing values
-    y_predicted = lin_reg.predict(X_poly)
-    y_test_predicted = lin_reg.predict(X_test_poly)
-
+    if deg == 10 :
+        from sklearn.linear_model import LinearRegression
+        lin_reg = LinearRegression()
+        lin_reg.fit(X_poly, y_train)
+        linear_train_score = lin_reg.score(X_poly, y_train)
+        linear_test_score = lin_reg.score(X_test_poly, y_test)
+        lasso_train_score = lasso.score(X_poly, y_train)
+        lasso_test_score = lasso.score(X_test_poly, y_test)
+        print(' Poly linear_train_score ', linear_train_score)
+        print(' Poly linear_test_score ', linear_test_score)
+        print(' Poly lasso_train_score ', lasso_train_score)
+        print(' Poly lasso_test_score ', lasso_test_score)
     from sklearn.metrics import mean_squared_error
-    #Getting RMSE
+    # Getting RMSE
     rmse_train = sqrt(mean_squared_error(y_train, y_predicted))
     rmse_test = sqrt(mean_squared_error(y_test, y_test_predicted))
 
-    #returning training rmse and test rmse
+    # returning training rmse and test rmse
     return rmse_train, rmse_test
+
 
 def main() :
 
@@ -36,28 +50,28 @@ def main() :
     X_train = train_dataset.iloc[:, 1:2].values
     y_train = train_dataset.iloc[:, 2].values
 
-    #Testing set
+    # Testing set
     X_test = test_dataset.iloc[:, 1:2].values
     y_test = test_dataset.iloc[:, 2].values
-
 
     max_deg = 10
     rmse_train = []
     rmse_test = []
-    for i in range(1,max_deg + 1) :
-        rmse_tr, rmse_ts = polynomial_regressor(i, X_train, X_test, y_train, y_test)
+    for i in range(1, max_deg + 1) :
+        rmse_tr, rmse_ts = polynomial_lasso_regressor(i, X_train, X_test, y_train, y_test)
         rmse_train.append(rmse_tr)
         rmse_test.append(rmse_ts)
     print(' RMSE Value for degree = 10 on training data', rmse_train[9])  
     print(' RMSE Value for degree = 10 on testing data', rmse_test[9])    
     plt.plot([i for i in range(1, max_deg + 1)], rmse_train, color="blue", label='train_data', marker='o', linestyle='solid')
     plt.plot([i for i in range(1, max_deg + 1)], rmse_test, color="red", label='test_data', marker='o', linestyle='solid')
-    plt.legend(loc='upper left')
+    plt.legend(loc='upper right')
     plt.title(" DEGREES-VS-ERMS ")
     plt.xlabel(" DEGREES ")
     plt.ylabel(" ERMS ")
-    plt.savefig('poly_deg_rmse.png')
+    plt.savefig('lasso_deg_rmse.png')
     plt.show()
+
 
 if __name__ == "__main__" :
     main()
--- 2018AIML600_regression_Part_D/2018aiml600_ridge_part_d.py
+++ 2018AIML600_regression_Part_D/2018aiml600_ridge_part_d.py
@@ -4,27 +4,45 @@ from math import sqrt
 
 from sklearn.preprocessing import PolynomialFeatures
 
-#Defining a polynomial regressor
-def polynomial_regressor(deg, X_train, X_test, y_train, y_test) :
-    poly_reg = PolynomialFeatures(degree = deg)
+
+# Defining a lasso polynomial regressor
+def polynomial_lasso_regressor(deg, X_train, X_test, y_train, y_test) :
+    poly_reg = PolynomialFeatures(degree=deg)
     X_poly = poly_reg.fit_transform(X_train)
     X_test_poly = poly_reg.fit_transform(X_test)
     poly_reg.fit(X_poly, y_train)
-    from sklearn.linear_model import LinearRegression
-    lin_reg = LinearRegression()
-    lin_reg.fit(X_poly, y_train)
+    # creating Ridge regressor with default alpha
+    from sklearn.linear_model import Ridge
+    ridge = Ridge()
+    ridge = Ridge()
+    ridge.fit(X_poly, y_train)
 
-    #Predicting training and testing values
-    y_predicted = lin_reg.predict(X_poly)
-    y_test_predicted = lin_reg.predict(X_test_poly)
+    print(' COEF :: ', ridge.coef_)
+    print(' INTERCEPT :: ', ridge.intercept_)
+    # Predicting training and testing values
+    y_predicted = ridge.predict(X_poly)
+    y_test_predicted = ridge.predict(X_test_poly)
 
+    if deg == 10 :
+        from sklearn.linear_model import LinearRegression
+        lin_reg = LinearRegression()
+        lin_reg.fit(X_poly, y_train)
+        linear_train_score = lin_reg.score(X_poly, y_train)
+        linear_test_score = lin_reg.score(X_test_poly, y_test)
+        ridge_train_score = ridge.score(X_poly, y_train)
+        ridge_test_score = ridge.score(X_test_poly, y_test)
+        print(' Poly linear_train_score ', linear_train_score)
+        print(' Poly linear_test_score ', linear_test_score)
+        print(' Poly ridge_train_score ', ridge_train_score)
+        print(' Poly ridge_test_score ', ridge_test_score)
     from sklearn.metrics import mean_squared_error
-    #Getting RMSE
+    # Getting RMSE
     rmse_train = sqrt(mean_squared_error(y_train, y_predicted))
     rmse_test = sqrt(mean_squared_error(y_test, y_test_predicted))
 
-    #returning training rmse and test rmse
+    # returning training rmse and test rmse
     return rmse_train, rmse_test
+
 
 def main() :
 
@@ -36,16 +54,15 @@ def main() :
     X_train = train_dataset.iloc[:, 1:2].values
     y_train = train_dataset.iloc[:, 2].values
 
-    #Testing set
+    # Testing set
     X_test = test_dataset.iloc[:, 1:2].values
     y_test = test_dataset.iloc[:, 2].values
-
 
     max_deg = 10
     rmse_train = []
     rmse_test = []
-    for i in range(1,max_deg + 1) :
-        rmse_tr, rmse_ts = polynomial_regressor(i, X_train, X_test, y_train, y_test)
+    for i in range(1, max_deg + 1) :
+        rmse_tr, rmse_ts = polynomial_lasso_regressor(i, X_train, X_test, y_train, y_test)
         rmse_train.append(rmse_tr)
         rmse_test.append(rmse_ts)
     print(' RMSE Value for degree = 10 on training data', rmse_train[9])  
@@ -56,8 +73,9 @@ def main() :
     plt.title(" DEGREES-VS-ERMS ")
     plt.xlabel(" DEGREES ")
     plt.ylabel(" ERMS ")
-    plt.savefig('poly_deg_rmse.png')
+    plt.savefig('ridge_deg_rmse.png')
     plt.show()
+
 
 if __name__ == "__main__" :
     main()
